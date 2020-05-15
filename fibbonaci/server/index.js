54c43eea7e623fd86787afa5e8164d3bb7f17797
Divyansh <divyanshkhandelwal147@gmail.com>
2020-05-16 02:46:47 +0530

    server setup with express routes

--- fibbonaci/server/index.js
+++ fibbonaci/server/index.js
@@ -24,3 +24,48 @@ pgClient.on('error', () => console.log('Lost PG Connection'))
 pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch(err => console.log(err))
+
+
+// Reddis Client Setup
+const redis = require('redis')
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate()
+
+
+//Express Route Handler
+
+app.get('/', (req, res) => {
+    res.send('hi').status(200)
+})
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values')
+    res.send(values.rows)
+})
+
+app.get('/values/current', (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values)
+    })
+    res.send(values.rows)
+})
+
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index Too High')
+    }
+    redisClient.hset('values', index, 'Nothing Yet!')
+    redisPublisher.publish('insert', index)
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+    res.send({ working: true })
+})
+
+app.listen(5000, err => {
+    console.log('Listening...')
+})
