25328942021a39c72e43ac062e73af706b61678a
Divyansh <divyanshkhandelwal147@gmail.com>
2020-05-17 02:52:17 +0530

    react app render functions

--- fibbonaci/client/src/fib.js
+++ fibbonaci/client/src/fib.js
@@ -23,4 +23,51 @@ class Fib extends Component {
             seenIndexes: seenIndexes.data
         })
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post('/api/values', {
+            index: this.state.index
+        });
+        this.setState({ index: '' });
+    }
+
+    renderSeenIndexed() {
+        return this.state.seenIndexes.map(({ number }) => number).join(', ')
+    }
+
+    renderValues() {
+        const entries = []
+        for (let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} I calculated {this.state.values[key]}
+                </div>
+            )
+        }
+
+        return entries;
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your index:</label>
+                    <input
+                        value={this.state.index}
+                        onChange={event => this.setState({ index: event.target.value })}
+                    />
+                    <button>Submit</button>
+                </form>
+                <h3>Index I saw:</h3>
+                {
+                    this.renderSeenIndexed()
+                }
+
+                <h3>Calculated Values:</h3>
+                {this.renderValues()}
+            </div>
+        )
+    }
 }
